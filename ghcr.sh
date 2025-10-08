6c04addf85aeabc54f214581e5b23ff6844ea2eb
myferr <contactme.myfer@protonmail.com>
2025-10-07 21:44:21 -0500

    chore: add ghcr.io push script

---
+++ ghcr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+V=$1
+
+if [ -z "$V" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+git tag -a "$V" -m "$V"
+git push origin "$V"
+
+docker build -t ghcr.io/myferr/deo:"$V" .
+docker tag ghcr.io/myferr/deo:"$V" ghcr.io/myferr/deo:latest
+docker push ghcr.io/myferr/deo:"$V"
+docker push ghcr.io/myferr/deo:latest
