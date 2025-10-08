46dd3ba74d470c9f81a26a32a3d569242d464359
myferr <contactme.myfer@protonmail.com>
2025-10-07 21:53:22 -0500

    feat: update images script

--- images.sh
+++ images.sh
@@ -11,7 +11,14 @@ fi
 git tag -a "$V" -m "$V"
 git push origin "$V"
 
+# GHCR
 docker build -t ghcr.io/myferr/deo:"$V" .
 docker tag ghcr.io/myferr/deo:"$V" ghcr.io/myferr/deo:latest
 docker push ghcr.io/myferr/deo:"$V"
 docker push ghcr.io/myferr/deo:latest
+
+# Docker Hub
+docker build -t myferr/deo:"$V" .
+docker tag myferr/deo:"$V" myferr/deo:latest
+docker push myferr/deo:"$V"
+docker push myferr/deo:latest
